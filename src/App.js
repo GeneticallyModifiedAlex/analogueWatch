e7d53bef244f8ed05e5f12cd728f1b9764be2241
Alex Edwards <aledw@netcompany.com>
2022-03-14 19:47:03 +0000

    setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Watch from './Components/watch.js';
+
+import "./stylesheets/reset.css";
+import "./stylesheets/watch.css";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Watch/>
+        
       </header>
     </div>
   );
